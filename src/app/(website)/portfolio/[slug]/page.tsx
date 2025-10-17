c3040dbc8f0dccf55a1b0e2a7b5f6c1fa6521f0e
Hemu-77 <gowdasphemanth@gmail.com>
2025-10-17 12:15:28 +0530

    pushed

--- src/app/(website)/portfolio/[slug]/page.tsx
+++ src/app/(website)/portfolio/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import VideoDetailClient from "@/Components/Portfolio/VideoDetails";
 
+interface PageProps {
+  params: { slug: string };
+}
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default function Page({ params }: PageProps) {
   return <VideoDetailClient slug={params.slug} />;
 }
